8262b799a7f7b13054b022a0762540a0163009d0
mt hasani <hasani.mohammadtaher@yahoo.com>
2021-04-24 02:35:52 +0430

    add py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 16 17:44:24 2021
+
+@author: mohammadtaher
+"""
+
+from setuptools import setup
+
+setup(name= 'vowelsearch', version= 1.2, py_module= ['vowelsearch'])
